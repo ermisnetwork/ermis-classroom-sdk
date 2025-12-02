37e9dd15113ee9d57795b849287f8d5bfc2cc156
monokaijs <monokaijs@gmail.com>
2025-12-02 14:44:01 +0700

    fix: refactor utils

--- packages/sdk/src/utils/index.ts
+++ packages/sdk/src/utils/index.ts
@@ -1,2 +1,9 @@
 export * from './browserDetection';
 export * from './signParticipantToken';
+
+export const formatMessage = (message: string): string => {
+  return `[SDK] ${message}`;
+};
+export const isValidApiKey = (apiKey: string): boolean => {
+  return apiKey.length > 0;
+};
